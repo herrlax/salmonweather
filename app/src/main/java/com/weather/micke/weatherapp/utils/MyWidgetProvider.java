52ceedfe28c29f977e66d94be929d51acf047b7f
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2015-02-26 20:50:38 +0100

    Refined model for better widget implementation.

--- app/src/main/java/com/weather/micke/weatherapp/utils/MyWidgetProvider.java
+++ app/src/main/java/com/weather/micke/weatherapp/utils/MyWidgetProvider.java
@@ -20,18 +20,11 @@ public class MyWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-        System.out.println("SNAKE CAKE!!!!!!");
+        System.out.println("NOW UPDATING WIDGET");
 
         for(int i = 0; i < appWidgetIds.length; i++) {
-            System.out.println(i + " time");
-            System.out.println("UPDATES!!********* ");
+            // getting current widget to update
             int appWidgetId = appWidgetIds[i];
-
-            // Create views from the layout which we want to update widget with
-
-
-            // TODO get layoutId (int)
-
 
             // Create an intent to launch the activity
             Intent intent = new Intent(context, WeatherActivity.class);
@@ -39,7 +32,23 @@ public class MyWidgetProvider extends AppWidgetProvider {
             //PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
             RemoteViews myNewView = new RemoteViews(context.getPackageName(), R.layout.wideget_layout);
 
-            //WeatherActivity weatherActivity = new WeatherActivity();
+            // WeatherActivity to fetch weather from
+            WeatherActivity weatherActivity = new WeatherActivity();
+            weatherActivity.getModel().setCity("gothenburg");
+
+            weatherActivity.fetchWeather();
+
+            int ost = 0;
+            while(ost < 999999999) {
+                ost++;
+                // waiting for valid weather data to be fetched
+                // not the most glorious way of doing this though..
+            }
+
+            myNewView.setTextViewText(R.id.cityTextView, weatherActivity.getModel().getCity());
+            myNewView.setTextViewText(R.id.tempTextView, weatherActivity.getModel().getTemp() + " °C");
+            myNewView.setImageViewResource(R.id.imageView, weatherActivity.getWeatherIcon());
+
             /*mainActivity.init();
             mainActivity.resetModel();
             mainActivity.setCity("Stockholm");
